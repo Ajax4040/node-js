6cceb485cc82e1138c716636078c22c9ea95d22d
Apx <apx.jul@gmail.com>
2024-03-16 22:39:27 -0300

    Reorganiza archivos de api-rest en la raíz del proyecto

--- common-js/sum.js
+++
@@ -1,7 +0,0 @@
-//Forma común y clasica de exportar un módulo llamada CommonJS
-function sum(a, b) {
-    return a + b;
-}
-
-module.exports = {sum}; // export the function sum
-  
--- mjs/index.mjs
+++
@@ -1,8 +0,0 @@
-//2) Forma de importar un módulo llamada ES6
-console.log('2) Forma de importar un módulo llamada ES6');
-import {sum, substract, multiply} from './sum.mjs';
-
-console.log(sum(2, 1));
-console.log(substract(2, 1));
-console.log(multiply(2, 1));
-
